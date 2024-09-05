2ea1d2585bddf0374b60b7cea29667d30cad46e9
milena579 <milecalegari@gmail.com>
2024-09-05 17:17:54 -0300

    feito ate a parte de estilização do fundo do card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
-export const Card = (props) => {
+import style from './Card.module.css'
+
+export const Card = ({name, desc, value, status, image}) => {
   return(
-      <div>
-          <h1>{props.name}</h1>
-          <h2>{props.desc}</h2>
-          <p>{props.value}</p>
-          <img src={props.image} alt={props.name} width={150} height={"auto"}/>
+      <div className={style.card}>
+          <h1>{name}</h1>
+          <h2>{desc}</h2>
+          <p>{value}</p>
+          <p className={style.status}>{`status ?  sta`}</p>
+          <img src={image} alt={name} width={150} height={"auto"}/>
       </div>
   )
 }
